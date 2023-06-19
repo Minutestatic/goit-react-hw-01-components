bdfda153f027ae2f259ba44ec39a6c8071cf1a81
Maksym Lahutin <lagutinmaksim.98@gmail.com>
2023-06-19 16:55:19 +0300

    creates a TransactionHistory component

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,9 +1,12 @@
-import Profile from "components/profile";
-import Statistics from "components/statistics";
-import FriendList from "components/friendList";
+import Profile from "components/Profile";
+import Statistics from "components/Statistics";
+import FriendList from "components/FriendList";
+import TransactionHistory from "components/TransactionHistory";
 import user from '../../data/user.json';
 import data from '../../data/data.json';
 import friends from '../../data/friends.json'
+import transactions from '../../data/transactions'
+
 
 
 
@@ -25,7 +28,8 @@ const App = () => {
       <Statistics  title="Upload stats" stats={data} />
       <Statistics stats={data} />
       
-      <FriendList friends={friends} />;
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions}/>
     </div>
   );
 };
