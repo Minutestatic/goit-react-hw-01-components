786682fc73192603e5c3d5afe8066da14861d171
Maksym Lahutin <lagutinmaksim.98@gmail.com>
2023-06-19 14:52:32 +0300

    fix component Statistics

--- src/components/statistics/Statistics.js
+++ src/components/statistics/Statistics.js
@@ -1,9 +1,8 @@
 const Statistics = props => {
   const { stats } = props;
-
   return (
     <section className="statistics">
-      <h2 className="title">Upload stats</h2>
+      {props.title && <h2 className="title">{props.title}</h2>}
 
       <ul className="stat-list">
         {stats.map(stat => (
